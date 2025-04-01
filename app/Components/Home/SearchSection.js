129794c4f4157ddaaf9b6e0bedc6f1d4ba925a99
Ajit <yajit1908@gmail.com>
2025-04-01 12:02:28 +0530

    Added the Source and Destination Context and Added lattiude or longitude

--- app/Components/Home/SearchSection.js
+++ app/Components/Home/SearchSection.js
@@ -1,12 +1,27 @@
-import React from 'react'
+"use client"
+import React, { useContext, useEffect } from 'react'
 import InputItem from './InputItem'
+import { SourceContext } from '@/context/SourceContext';
+import { DestinationContext } from '@/context/DestinationContext';
 
 const SearchSection = () => {
+      const {source,setSource}=useContext(SourceContext);
+      const {destination,setDestination}=useContext(DestinationContext);
+     useEffect(()=>{
+        if(source){
+          console.log(source);
+        }
+        if(destination){
+          console.log(destination);
+        }
+     },
+    [source,destination])
+
   return (
     <div className='p-2 md:pd-5 border-[2px] rounded-xl'>
       <p className='text-[20px] font-bold'>Get a Ride</p>
       <InputItem type="source"/>
-      <InputItem type = "dest"/>
+      <InputItem type = "destination"/>
       <button className='p-3 bg-black w-full mt-5 text-white 
       rounded-lg hover:cursor-grab'>Search</button>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,15 +1,26 @@
+"use client"
 import SearchSection from "./Components/Home/SearchSection";
 import GoogleMapSection from "./Components/Home/GoogleMapSection";
+import { SourceContext } from "@/context/SourceContext";
+import { useState } from "react";
+import { DestinationContext } from "@/context/DestinationContext";
 
 export default function Home() {
+  const [source,setSource]=useState([]);
+  const [destination , setDestination] = useState([]); 
   return (
+  
+    <SourceContext.Provider value={{source,setSource}}>
+      <DestinationContext.Provider value={{destination,setDestination}}>
      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
-       <div className="">
+       <div>
          <SearchSection/>
        </div>
         <div className="col-span-2">
           <GoogleMapSection/>
         </div>
      </div>
+     </DestinationContext.Provider>
+     </SourceContext.Provider>
   );
 }
